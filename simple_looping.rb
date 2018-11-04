2a26b6865ae3bef5510d890a51afced0143972cf
Samuel Prada <jspradac@gmail.com>
2018-11-04 21:31:25 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -32,6 +32,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   stop = 0
   while stop < number_of_times 
+   
 end
 
 def until_iterator(number_of_times)
